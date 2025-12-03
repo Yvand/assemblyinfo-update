ff9a951729d11eb782bcc01456df96897039f43e
Yvan Duhamel <yvandev@outlook.fr>
2025-12-03 15:02:05 +0100

    only 1 input version

--- SetVersion.ps1
+++ SetVersion.ps1
@@ -1,15 +1,14 @@
 $directory = $Env:DIRECTORY
 $fileName = $Env:FILENAME
-$assemblyVersion = $Env:ASSEMBLY_VERSION
-$assemblyFileVersion = $Env:ASSEMBLY_FILE_VERSION
+$version = $Env:VERSION
 $copyright = $Env:COPYRIGHT
 $recursive = ([String]::IsNullOrEmpty($Env:RECURSIVE)) ? $false : [System.Convert]::ToBoolean($Env:RECURSIVE)
 $githubOutput = $Env:GITHUB_OUTPUT
+$assemblyFileVersion = $version
 
-function SetVersion($file)
-{
+function SetVersion($file) {
 	$contents = [System.IO.File]::ReadAllText($file.FullName)
-	$contents = [Regex]::Replace($contents, '(AssemblyVersion\(").*("\)])', "`${1}$assemblyVersion`${2}", [System.Text.RegularExpressions.RegexOptions] "Multiline, IgnoreCase")
+	$contents = [Regex]::Replace($contents, '(AssemblyVersion\(").*("\)])', "`${1}$version`${2}", [System.Text.RegularExpressions.RegexOptions] "Multiline, IgnoreCase")
 	$contents = [Regex]::Replace($contents, '(AssemblyFileVersion\(").*("\)])', "`${1}$assemblyFileVersion`${2}", [System.Text.RegularExpressions.RegexOptions] "Multiline, IgnoreCase")
 	if ($false -eq [String]::IsNullOrEmpty($copyright)) {
 		$contents = [Regex]::Replace($contents, '(AssemblyCopyright\(").*("\)])', "`${1}$copyright`${2}", [System.Text.RegularExpressions.RegexOptions] "Multiline, IgnoreCase")
@@ -19,21 +18,30 @@ function SetVersion($file)
 	$streamWriter.Write($contents)
 	$streamWriter.Close()
 
-	Write-Output "assemblyVersion=$assemblyVersion" >> $githubOutput
+	Write-Output "assemblyVersion=$version" >> $githubOutput
 	Write-Output "assemblyFileVersion=$assemblyFileVersion" >> $githubOutput
-	Write-Host "$file updated with assemblyVersion '$assemblyVersion' and assemblyFileVersion '$assemblyFileVersion'"
+	Write-Host "$file updated with assemblyVersion '$version' and assemblyFileVersion '$assemblyFileVersion'"
 }
 
-if ($recursive)
-{
+$patternExactSemver = '^\d+\.\d+(\.\d+)*$'
+$patternStartsWithSemver = '^\d+\.\d+(\.\d+)*'
+$isSemVer = [Regex]::Match($version, $patternExactSemver)
+if ($false -eq $isSemVer.success) {
+	$startsWithSemVer = [Regex]::Match($version, $patternStartsWithSemver)
+	if ($startsWithSemVer.success) {
+		$version = $startsWithSemVer.Value
+	} else {
+		Write-Host "Version number '$version' is invalid for use in assembly info versions"
+	}
+}
+
+if ($recursive) {
 	$assemblyInfoFiles = Get-ChildItem $directory -Recurse -Include $fileName
-	foreach($file in $assemblyInfoFiles)
-	{	
+	foreach ($file in $assemblyInfoFiles) {	
 		SetVersion($file)
 	}
 }
-else
-{
+else {
 	$file = Get-ChildItem $directory -Filter $fileName | Select-Object -First 1
 	SetVersion($file)
 }
